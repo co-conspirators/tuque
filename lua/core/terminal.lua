5342eca9f63919a0a50b28134212d9334c615dae
Gabe Dunn <gabe@sent.at>
2024-07-23 19:20:49 -0600

    feat: improve toggleterm config

--- lua/core/terminal.lua
+++ lua/core/terminal.lua
@@ -1,5 +1,16 @@
 MainTerminal = nil
 
+local toggle_term = function()
+	if not MainTerminal:is_open() then
+		MainTerminal:open()
+	elseif not MainTerminal:is_focused() then
+		MainTerminal:focus()
+		vim.cmd('startinsert') -- switch to terminal mode
+	else
+		vim.cmd('wincmd p')
+	end
+end
+
 return {
 	-- nested neovim instances open in the parent
 	{
@@ -14,53 +25,45 @@ return {
 		'akinsho/nvim-toggleterm.lua',
 		keys = {
 			{
-				'<leader>t',
-				function()
-					if not MainTerminal:is_open() then
-						MainTerminal:open(80, 'vertical')
-					elseif not MainTerminal:is_focused() then
-						MainTerminal:focus()
-						vim.cmd('startinsert') -- switch to terminal mode
-					else
-						vim.cmd('wincmd p')
-					end
-				end,
+				[[<C-\>]],
+				toggle_term,
 				desc = 'Focus terminal',
 			},
 			{
-				'<leader>T',
-				function()
-					if MainTerminal:is_open() then
-						MainTerminal:close()
-					else
-						MainTerminal:open(100, 'vertical')
-					end
-				end,
-				desc = 'Toggle terminal',
+				'<leader>t',
+				toggle_term,
+				desc = 'Focus terminal',
 			},
 		},
-		config = function()
-			require('toggleterm').setup({})
+		opts = {
+			autochdir = true,
+			auto_scroll = false,
+			shade_terminals = false,
+			start_in_insert = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			open_mapping = [[<C-\>]],
 
-			MainTerminal = require('toggleterm.terminal').Terminal:new({
-				auto_scroll = false,
-				autochdir = true,
-				-- HACK: solves an issue with barbecue.nvim displaying a window bar
-				-- https://github.com/utilyre/barbecue.nvim/issues/92
-				on_open = function(term)
-					vim.defer_fn(function()
-						vim.wo[term.window].winbar = ''
-					end, 0)
-				end,
-			})
+			-- HACK: solves an issue with barbecue.nvim displaying a window bar
+			-- https://github.com/utilyre/barbecue.nvim/issues/92
+			on_open = function(term)
+				vim.defer_fn(function()
+					vim.wo[term.window].winbar = ''
+				end, 0)
+			end,
+		},
+		config = function(_, opts)
+			require('toggleterm').setup(opts)
 
-			local opts = { buffer = 0 }
-			vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-			vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-			vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-			vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-			vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-			vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+			MainTerminal = require('toggleterm.terminal').Terminal:new()
+
+			local mapOpts = {}
+			vim.keymap.set('t', 'jk', [[<C-\><C-n>]], mapOpts)
+			vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], mapOpts)
+			vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], mapOpts)
+			vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], mapOpts)
+			vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], mapOpts)
+			vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], mapOpts)
 
 			-- We can use <esc><esc> to be able to use a single <esc> in the terminal Vi mode.
 			-- vim.api.nvim_buf_set_keymap(0, "t", "<esc><esc>", [[<C-\><C-o>:ToggleTerm<CR>]], { noremap = true })
