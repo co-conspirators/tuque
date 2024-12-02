a9baee1e74a2b44ca74c039b0e0280ddf00f475f
Liam Dyer <liamcdyer@gmail.com>
2024-12-01 20:27:34 -0500

    feat: switch to snacks.nvim profiler

--- lua/profile/profile.lua
+++ lua/profile/profile.lua
@@ -1,46 +1,24 @@
+-- TODO: startup profiling
 return {
 	enabled = os.getenv('NVIM_PROFILE') ~= nil,
-	'stevearc/profile.nvim',
-	config = function()
-		local should_profile = os.getenv('NVIM_PROFILE')
-		if should_profile then
-			local profile = require('profile')
-			profile.instrument_autocmds()
+	'folke/snacks.nvim',
+	lazy = false,
+	priority = 1000,
+	opts = function()
+		local Snacks = require('snacks')
+		-- Toggle the profiler
+		Snacks.toggle.profiler():map('<f1>')
+		-- Toggle the profiler highlights
+		Snacks.toggle.profiler_highlights():map('<f2>')
 
-			local range = os.getenv('NVIM_PROFILE')
-			if range == '1' or range == 'start' then
-				range = '*'
-			end
-
-			profile.ignore('vim.tbl_*')
-			profile.ignore('vim.shared.*')
-			profile.ignore('vim._editor.*')
-			profile.ignore('blink.cmp.windows.lib.render.*')
-			profile.ignore('blink.cmp.windows.render_item*')
-			profile.instrument(range)
-			if should_profile:lower():match('^start') then
-				profile.start(range)
-			end
-		end
-
-		local function toggle_profile()
-			if not should_profile then
-				vim.notify('Set NVIM_PROFILE to enable profiling', 'warn')
-				return
-			end
-			local prof = require('profile')
-			if prof.is_recording() then
-				prof.stop()
-				vim.ui.input({ prompt = 'Save profile to:', completion = 'file', default = 'profile.json' }, function(filename)
-					if filename then
-						prof.export(filename)
-						vim.notify(string.format('Wrote %s', filename))
-					end
-				end)
-			else
-				prof.start('*')
-			end
-		end
-		vim.keymap.set('', '<f1>', toggle_profile)
+		return {
+			profiler = {
+				filter_mod = {
+					default = false,
+					['^blink.cmp%.'] = true,
+					['^blink.cmp.completion.windows.render%.'] = false,
+				},
+			},
+		}
 	end,
 }
